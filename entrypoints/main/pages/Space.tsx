a31514886aa06541ee6cb9494924eb3912cea5a9
Zhi.Huang <zh.impact@outlook.com>
2025-01-16 06:04:44 +0800

    update:

--- entrypoints/main/pages/Space.tsx
+++ entrypoints/main/pages/Space.tsx
@@ -6,6 +6,13 @@ import { useStore } from "../store";
 export default function Spaces() {
   const currentWindow = useStore((state) => state.currentWindow);
 
+  const handleTabClick = (tab: chrome.tabs.Tab) => {
+    browser.windows.update(tab.windowId ?? browser.windows.WINDOW_ID_CURRENT, {
+      focused: true,
+    });
+    browser.tabs.update(tab.id ?? 0, { active: true });
+  };
+
   return (
     <div className="">
       <h2>Spaces: </h2>
@@ -13,7 +20,7 @@ export default function Spaces() {
         {currentWindow?.tabs?.map((tab) => (
           <li key={tab.id} className="flex gap-4 items-center">
             <Avatar src={tab.favIconUrl} size="xs" />
-            <p className="cursor-pointer">{tab.title}</p>
+            <p className="cursor-pointer" onClick={() => handleTabClick(tab)}>{tab.title}</p>
             {/* <Button onClick={() => browser.tabs.remove(tab.id ?? 0)}>close</Button> */}
           </li>
         ))}
